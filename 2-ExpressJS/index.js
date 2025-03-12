223d8fb8bf212c8b7fc74a8ee5b6d7ee3dd2f2d7
amareshka <amareshbevoor@gmail.com>
2025-03-12 17:36:32 +0530

    Nodemon package installation

--- 2-ExpressJS/index.js
+++ 2-ExpressJS/index.js
@@ -9,6 +9,10 @@ app.get("/about", (req, res) => {
   res.send("This is from About page..");
 });
 
+app.get("/contact", (req, res) => {
+  res.send("This is from Contact page..");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`We are listening to ${PORT}`));

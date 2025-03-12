b1a12847c9728c4fd4151f23bf76f11912af0868
amareshka <amareshbevoor@gmail.com>
2025-03-12 18:13:46 +0530

    Usage of routing parameters to display courses by id

--- 2-ExpressJS/index.js
+++ 2-ExpressJS/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "HTML" },
+  { id: 2, name: "CSS" },
+  { id: 3, name: "Javavscript" },
+  { id: 4, name: "React JS" },
+  { id: 5, name: "Node JS" }
+];
+
 app.get("/", (req, res) => {
   res.send("This is from Express JS..");
 });
@@ -12,6 +20,15 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.send("This is from Contact page..");
 });
+
+//Route Parameters
+
+app.get("/courses/:id", (req,res)=>{
+    let course = courses.find(course=> course.id === parseInt(req.params.id));
+    
+    if(!course) res.status(404).send("The course that you have searched does not exists..")
+    res.send(course);
+})
 
 const PORT = process.env.PORT || 3000;
 

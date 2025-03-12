7aa12cfa6d188c6ca95f29928a713ca9adf91444
amareshka <amareshbevoor@gmail.com>
2025-03-12 19:12:25 +0530

    Fetching courses and adding new course

--- 2-ExpressJS/index.js
+++ 2-ExpressJS/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "HTML" },
   { id: 2, name: "CSS" },
   { id: 3, name: "Javavscript" },
   { id: 4, name: "React JS" },
-  { id: 5, name: "Node JS" }
+  { id: 5, name: "Node JS" },
 ];
 
 app.get("/", (req, res) => {
@@ -23,12 +25,31 @@ app.get("/contact", (req, res) => {
 
 //Route Parameters
 
-app.get("/courses/:id", (req,res)=>{
-    let course = courses.find(course=> course.id === parseInt(req.params.id));
-    
-    if(!course) res.status(404).send("The course that you have searched does not exists..")
-    res.send(course);
-})
+app.get("/courses/:id", (req, res) => {
+  let course = courses.find((course) => course.id === parseInt(req.params.id));
+
+  if (!course)
+    res.status(404).send("The course that you have searched does not exists..");
+  res.send(course);
+});
+
+// Get all the courses
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+// Add new course
+
+app.post("/courses/", (req, res) => {
+  const newCourse = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(newCourse);
+  res.send(newCourse);
+});
 
 const PORT = process.env.PORT || 3000;
 
